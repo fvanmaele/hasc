69fa9e806ab8fdd3fc264591ea8724190821de5e
Ferdinand Vanmaele <fvanmaele@outlook.de>
2021-04-30 20:06:45 +0200

    use horner scheme

--- ex01/03_numerical_integration/functions.hh
+++ ex01/03_numerical_integration/functions.hh
@@ -2,6 +2,7 @@
 #define HASC_EX01_FUNCTIONS_HH
 #include <cmath>
 #include <vectorclass/vectorclass.h>
+#define __forceinline inline __attribute__((always_inline))
 
 namespace hasc_ex01
 {
@@ -10,14 +11,29 @@ struct Func1
 {
     double operator()(double x)
     {
-        return x*x*x - 2*x*x + 3*x - 1;
+        return x*(x*(x - 2) + 3) - 1;
     }
     Vec4d operator()(Vec4d x)
     {
-        return x*x*x - 2*x*x + 3*x - 1;
+        return x*(x*(x - 2) + 3) - 1;
     }
 };
 
+// f(x) = x^0 + x + ... + x^15
+struct Func2
+{
+    double operator()(double x)
+    {
+        return (x + 1) * (x*x + 1) * (x*x*x*x + 1) * (x*x*x*x*x*x*x*x + 1);
+    }
+    Vec4d operator()(Vec4d x)
+    {
+        Vec4d ones = Vec4d(1, 1, 1, 1);
+        return (x + ones) * (x*x + ones) * (pow_const(x, 4) + 1) * (pow_const(x, 8) + 1);
+    }
+};
+
+// TODO: unoptimized comparison versions
 // F(x) = 1/4 x^4  - 2/3 x^3 + 3/2 x^2 - x
 struct Prim1
 {
@@ -28,27 +44,6 @@ struct Prim1
     Vec4d operator()(Vec4d x)
     {
         return pow_const(x, 4)/4 - 2*pow_const(x, 3)/3 + 3*x*x/2 - x;
-    }
-};
-
-// f(x) = x^0 + x + ... + x^15
-struct Func2
-{
-    double operator()(double x)
-    {
-        double sum = 1 + x;
-        for (int i = 2; i <= 15; ++i) {
-            sum += std::pow(x, i);
-        }
-        return sum;
-    }
-    Vec4d operator()(Vec4d x)
-    {
-        // pow_const has "often better" efficiency than pow, so no loops here
-        return Vec4d(1, 1, 1, 1) + x + pow_const(x, 2) + pow_const(x, 3) + pow_const(x, 4) +
-               pow_const(x, 5) + pow_const(x, 6) + pow_const(x, 7) + pow_const(x, 8) +
-               pow_const(x, 9) + pow_const(x, 10) + pow_const(x, 11) + pow_const(x, 12) +
-               pow_const(x, 13) + pow_const(x, 14) + pow_const(x, 15);
     }
 };
 
@@ -65,7 +60,6 @@ struct Prim2
     }
     Vec4d operator()(Vec4d x)
     {
-        // pow_const has "often better" efficiency than pow, so no loops here
         return Vec4d(1, 1, 1, 1) + x + pow_const(x, 2)/2 + pow_const(x, 3)/3 + pow_const(x, 4)/4 +
                pow_const(x, 5)/5 + pow_const(x, 6)/6 + pow_const(x, 7)/7 + pow_const(x, 8)/8 +
                pow_const(x, 9)/9 + pow_const(x, 10)/10 + pow_const(x, 11)/11 + pow_const(x, 12)/12 +
