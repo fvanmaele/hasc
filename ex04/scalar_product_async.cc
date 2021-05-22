4ae51dd05e61c36cd14a83ac057323f776f04555
Ferdinand Vanmaele <fvanmaele@mailbox.org>
2021-05-22 11:49:15 +0200

    init ex04

---
+++ ex04/scalar_product_async.cc
@@ -0,0 +1,8 @@
+#include <future>
+#include <thread>
+#include <algorithm>
+#include <benchmark/benchmark.h>
+
+int main() {
+    return 0;
+}
---
+++ ex04/tree_barrier.cc
@@ -0,0 +1,5 @@
+#include "Barrier.hh"
+
+int main() {
+    return 0;
+}
